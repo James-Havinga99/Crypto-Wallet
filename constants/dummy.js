46d130f2b63967e5a1c9ca6963243bf06db0923a
James-Havinga99 <Jameshav132@gmail.com>
2021-07-19 17:49:09 +0200

    full-project

---
+++ constants/dummy.js
@@ -0,0 +1,35 @@
+export const holdings = [
+  {
+    id: "bitcoin",
+    qty: 1,
+  },
+  {
+    id: "ethereum",
+    qty: 10,
+  },
+  {
+    id: "dogecoin",
+    qty: 420,
+  },
+];
+
+export const profile = {
+  id: 8888888,
+  email: "Jameshav132@gmail.com",
+};
+
+export const settings = {
+  launchScreen: "Home",
+  currency: "USD",
+  appearance: "Dark",
+  language: "English",
+  faceId: true,
+};
+
+const dummyData = {
+  holdings,
+  profile,
+  settings,
+};
+
+export default dummyData;
